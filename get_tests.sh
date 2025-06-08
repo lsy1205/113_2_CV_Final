d5ef564b7107dffff197539d1ac37222b4afdee2
lsy1205 <stevenliu901205@gmail.com>
2025-06-08 23:38:19 +0800

    add download file

---
+++ get_tests.sh
@@ -0,0 +1,12 @@
+
+# test
+gdown "https://drive.google.com/uc?id=1KwOFpFwUb7etLhWumU2tlddPdCtfbBLE"
+unzip test.zip -d test
+
+# test_dust
+gdown "https://drive.google.com/uc?id=18kLuRAjp1hbrg-PbS7sz0A0Oa2_8dumO"
+unzip test_dust.zip -d test_dust
+
+# test_dust_transformer
+gdown "https://drive.google.com/uc?id=19p4kHxNMbWyqswIoDnPa9Eo8iX2HZc26"
+unzip test_dust_transformer.zip -d test_dust_transformer
---
+++ source/Transformer/get_checkpoint.sh
@@ -0,0 +1,4 @@
+mkdir -p checkpoints_test
+cd checkpoints_test
+gdown "https://drive.google.com/uc?id=1x4kNHV9WxZM-rtIjYwzFLSphmXgcxBJs"
+cd ..
