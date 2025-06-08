a5c9bb2dc86b9aab99e4fe92bd2d1b6c690d4025
AndrePengPeng <pengboshiang@gmail.com>
2025-06-08 23:47:24 +0800

    get_test.sh

--- get_tests.sh
+++ get_tests.sh
@@ -1,12 +1,12 @@
 
 # test
 gdown "https://drive.google.com/uc?id=1KwOFpFwUb7etLhWumU2tlddPdCtfbBLE"
-unzip test.zip -d test
+unzip test.zip -d .
 
 # test_dust
 gdown "https://drive.google.com/uc?id=18kLuRAjp1hbrg-PbS7sz0A0Oa2_8dumO"
-unzip test_dust.zip -d test_dust
+unzip test_dust.zip -d t.
 
 # test_dust_transformer
 gdown "https://drive.google.com/uc?id=19p4kHxNMbWyqswIoDnPa9Eo8iX2HZc26"
-unzip test_dust_transformer.zip -d test_dust_transformer
+unzip test_dust_transformer.zip -d .
