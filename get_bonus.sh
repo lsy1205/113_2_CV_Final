b56b83e7ab6b6afc717ccf02087e0c25d19d0073
lsy1205 <stevenliu901205@gmail.com>
2025-06-09 00:10:44 +0800

    update download shell scripts

--- get_bonus.sh
+++ get_bonus.sh
@@ -2,3 +2,4 @@
 # test
 gdown "https://drive.google.com/uc?id=1yuvF-4IQYOnURJtV-MID2GIWL5OPe7W_"
 unzip bonus.zip -d .
+rm bonus.zip
--- get_tests.sh
+++ get_tests.sh
@@ -2,11 +2,14 @@
 # test
 gdown "https://drive.google.com/uc?id=1KwOFpFwUb7etLhWumU2tlddPdCtfbBLE"
 unzip test.zip -d .
+rm test.zip
 
 # test_dust
 gdown "https://drive.google.com/uc?id=18kLuRAjp1hbrg-PbS7sz0A0Oa2_8dumO"
-unzip test_dust.zip -d t.
+unzip test_dust.zip -d .
+rm test_dust.zip
 
 # test_dust_transformer
 gdown "https://drive.google.com/uc?id=19p4kHxNMbWyqswIoDnPa9Eo8iX2HZc26"
 unzip test_dust_transformer.zip -d .
+rm test_dust_transformer.zip
